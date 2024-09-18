94afdb38b4b1e1b126ca0ebaec4d15464bbb383b
Gatess Zi vest <filiptrajceski69@gmail.com>
2024-09-18 18:19:11 +0200

    Changed a letter for testing purposes

--- CryptoSphere.Wallet/CryptoSphere.Wallet.Api/Controllers/WeatherForecastController.cs
+++ CryptoSphere.Wallet/CryptoSphere.Wallet.Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace CryptoSphere.Wallet.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Frezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
