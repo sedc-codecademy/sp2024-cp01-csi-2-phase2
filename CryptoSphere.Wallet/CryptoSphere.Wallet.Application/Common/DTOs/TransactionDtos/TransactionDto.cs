ff22b69259a11e42dd1440800b9f61b36929a4a4
Filip Trajceski <153946352+FilipTrajceski@users.noreply.github.com>
2024-10-01 19:42:47 +0200

    Delete CryptoSphere.Wallet directory

--- CryptoSphere.Wallet/CryptoSphere.Wallet.Application/Common/DTOs/TransactionDtos/TransactionDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CryptoSphere.Wallet.Application.Common.DTOs.TransactionDtos
-{
-    public class TransactionDto : BaseTransactionDto
-    {
-        public int WalletId {  get; set; }
-    }
-}
