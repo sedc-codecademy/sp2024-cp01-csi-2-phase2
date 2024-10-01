b738b15ff8d5bd221963ee32633693b097888547
Filip Trajceski <153946352+FilipTrajceski@users.noreply.github.com>
2024-10-01 19:55:13 +0200

    Add files via upload

---
+++ CryptoSphere.Wallet.Infrastructure/DataLayer/FluentConfig/CryptoCoinFluentConfig.cs
@@ -0,0 +1,27 @@
+﻿using CryptoSphere.Wallet.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CryptoSphere.Wallet.Infrastructure.DataLayer.FluentConfig
+{
+    public class CryptoCoinFluentConfig : IEntityTypeConfiguration<CryptoCoin>
+    {
+        public void Configure(EntityTypeBuilder<CryptoCoin> builder)
+        {
+            builder.HasKey(x => x.CoinId);
+
+            builder.HasOne(x => x.Wallet)
+                .WithMany(x => x.CryptoCoins)
+                .HasForeignKey(x => x.WalletId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.Property(x => x.CoinSymbol).IsRequired().HasConversion<int>();
+
+            builder.Property(x => x.Quantity).IsRequired().HasPrecision(5, 2);
+
+            builder.Property(x => x.ValueInUSD).IsRequired().HasPrecision(5, 2);
+
+        }
+    }
+}
---
+++ CryptoSphere.Wallet.Infrastructure/Migrations/20241001130007_shadowPropertyTryFix.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoSphere.Wallet.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class shadowPropertyTryFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Wallets",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 10, 1, 13, 0, 6, 598, DateTimeKind.Utc).AddTicks(5152),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 10, 1, 12, 51, 54, 446, DateTimeKind.Utc).AddTicks(8879));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TransactionDate",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 10, 1, 15, 0, 6, 597, DateTimeKind.Local).AddTicks(7307),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 10, 1, 14, 51, 54, 445, DateTimeKind.Local).AddTicks(8812));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Wallets",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 10, 1, 12, 51, 54, 446, DateTimeKind.Utc).AddTicks(8879),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 10, 1, 13, 0, 6, 598, DateTimeKind.Utc).AddTicks(5152));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TransactionDate",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 10, 1, 14, 51, 54, 445, DateTimeKind.Local).AddTicks(8812),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 10, 1, 15, 0, 6, 597, DateTimeKind.Local).AddTicks(7307));
+        }
+    }
+}
